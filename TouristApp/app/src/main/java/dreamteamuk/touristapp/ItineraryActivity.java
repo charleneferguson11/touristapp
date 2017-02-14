7c646c8c0b9a9ba936d6308dda3b649df562e9cd
Charlene <charlenec11@yahoo.com.au>
2017-02-14 00:06:20 +0000

    Add android project

---
+++ TouristApp/app/src/main/java/dreamteamuk/touristapp/ItineraryActivity.java
@@ -0,0 +1,13 @@
+package dreamteamuk.touristapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItineraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerary);
+    }
+}
