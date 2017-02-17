51528fc7a9c44e18a7859603c07150f8b2f64317
Charlene <charlenec11@yahoo.com.au>
2017-02-17 22:10:57 +0000

    Add layout manger

--- TouristApp/app/src/main/java/dreamteamuk/touristapp/ItineraryActivity.java
+++ TouristApp/app/src/main/java/dreamteamuk/touristapp/ItineraryActivity.java
@@ -3,11 +3,16 @@ package dreamteamuk.touristapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class ItineraryActivity extends AppCompatActivity {
-
+    // Number of items the view can hold
     private static final int NUMBER_OF_ITEMS = 50;
-    private 
+    // member that holds the adapter for the itinerary list
+    private ItineraryAdapter mAdapter;
+    // member that holds recyclerview of the itinerary list
+    private RecyclerView mItineraryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,14 @@ public class ItineraryActivity extends AppCompatActivity {
 
         // Add button to add Itinerary items
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        mItineraryList = (RecyclerView) findViewById(R.id.rv_itinerary);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mItineraryList.setLayoutManager(layoutManager);
+
+        mAdapter = new ItineraryAdapter(NUMBER_OF_ITEMS);
+        mItineraryList.setAdapter(mAdapter);
     }
 }
