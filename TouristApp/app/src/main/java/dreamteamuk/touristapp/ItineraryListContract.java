3efd0dee0621325c018e0c43fe27a14e5c8fe5df
Charlene <charlenec11@yahoo.com.au>
2017-02-17 23:23:37 +0000

    Start database configuration contract class

---
+++ TouristApp/app/src/main/java/dreamteamuk/touristapp/ItineraryListContract.java
@@ -0,0 +1,18 @@
+package dreamteamuk.touristapp;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by charlene on 17/02/2017.
+ */
+public class ItineraryListContract {
+    public class ItineraryListEntry implements BaseColumns {
+        // TABLE_NAME is itinerarylist
+        public static final String TABLE_NAME = "itinerarylist";
+        // COLUMN_PLACE_NAME is placeName
+        public static final String COLUMN_PLACE_NAME = "placeName";
+        // COLUMN_PRIORITY is priority
+        public static final String COLUMN_PRIORITY = "priority";
+
+    }
+}
