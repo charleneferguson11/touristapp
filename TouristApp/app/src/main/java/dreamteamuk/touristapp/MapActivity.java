5ce17a210a0c2a05cdbc8e78dd6127c87dda9b8a
Charlene <charlenec11@yahoo.com.au>
2017-03-12 08:23:39 +0000

    Add map and markers

--- TouristApp/app/src/main/java/dreamteamuk/touristapp/MapActivity.java
+++ TouristApp/app/src/main/java/dreamteamuk/touristapp/MapActivity.java
@@ -1,18 +1,32 @@
 package dreamteamuk.touristapp;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Marker mMarker;
+    LatLng britishMuseum = new LatLng(51.519788, -0.126976);
+    LatLng nationalGallery = new LatLng(51.509485, -0.128560);
+    LatLng nationalHistoryMuseum = new LatLng(51.496869, -0.176356);
+    LatLng londonUk = new LatLng(51.535995, -0.129100);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +53,87 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        try {
+            getGeoCoordinates();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    * This method creates a latitude and longitude object
+    * Updates the camera on the map
+    * This method positions the camera on the map using latitude,
+    * longitude and the zoom level of the camera.
+    * @param latitude is coordinate for latitude
+    * @param longitude is coordinate for longitude
+    * @param zoom  is the zoom level for the camera
+    * */
+
+    public void addMapLocation(double latitude, double longitude, float zoom) {
+        // Create a latlng object
+        LatLng aLocation = new LatLng(latitude, longitude);
+        //
+        CameraUpdate updateMapCamera = CameraUpdateFactory.newLatLngZoom(aLocation, zoom);
+        // Sets the initial state of the camera
+        mMap.moveCamera(updateMapCamera);
+    }
+
+    /*
+    * This method adds a custom marker to the map.
+    * @param address: Address object from geocoder api
+    * @param lat: Latitude
+    * @param lng: Longitude
+    *
+    * */
+    public void addCustomMarker(Address address, double lat, double lng) {
+        MarkerOptions options = new MarkerOptions()
+                .title(address.getLocality()).position(new LatLng(lat,lng))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+        // Add country to info window if available.
+        String country = address.getCountryName();
+        if(country.length() > 0){
+            options.snippet(country);
+        }
+
+        mMarker = mMap.addMarker(options);
+
+    }
+
+    /*
+    * Implement geocoder api to obtain an Address from a name or postcode search
+    * Once we have the address object we obtain the name and latitude and longitude
+    * coordinates.
+    * */
+    public void getGeoCoordinates() throws IOException {
+
+        String searchString = "London";
+
+        Geocoder gc = new Geocoder(this);
+        //Get a list of address objects based on the search string and set number of results to 1
+        List<Address> list = gc.getFromLocationName(searchString, 1);
+
+        if (list.size() > 0) {
+            Address address = list.get(0);
+            String localityName = address.getLocality();
+            double addressLatitude = address.getLatitude();
+            double addressLongitude = address.getLongitude();
+            // Call the method that positions the camera on the map
+            addMapLocation(addressLatitude, addressLongitude, 15);
+            // Set up the marker's name, colour and position.
+            MarkerOptions options = new MarkerOptions()
+                    .title(localityName)
+                    .position(new LatLng(addressLatitude, addressLongitude))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+            // Add a marker to the map
+            mMarker = mMap.addMarker(options);
+
+        }
+
     }
 }
